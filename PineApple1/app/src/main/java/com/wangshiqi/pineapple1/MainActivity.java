c5109a01c412e59fb224706b26ef8cefd024431c
dllo <812913819@qq.com>
2016-10-17 11:29:56 +0800

    整合统一

--- PineApple1/app/src/main/java/com/wangshiqi/pineapple1/MainActivity.java
+++ PineApple1/app/src/main/java/com/wangshiqi/pineapple1/MainActivity.java
@@ -8,66 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//  我的功能
-
-        // 王林的分支
-
-
-        /**
-         *  董魁
-
-        // 王世琦的分支
-        // 1231425465465
-
-
-        // 你好啊 
-
-
-
-        /**
-         *  你在干什么 好乱呀 
-         */
-
-
-        /**
-         *  再来一遍
-         */
-
-
-        /***
-         *  不对呀 
-
-        /**
-         * 好烦
-         */
-
-        /**
-         * 啊啊啊啊啊
-         */
-        // 你好啊
-
-
-
-        // 世界真小
-
-
-        /***
-         *
-         * ed
-         */
-
-
-        /************大哥懂了************/
-        /**
-         * 好乱  懵逼了
-         */
-
-        /**
-         *  卡卡卡
-         */
-        // 要炸了
-        /**
-         * 大致会了
-         */
+        setContentView(R.layout.activity_main);
     }
 }
